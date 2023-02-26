6c9c089ee78cf6fc91da93c5f61f3334808af838
Deepak Yadav <dy22900@gmail.com>
2023-02-26 11:53:31 +0530

    first commit

---
+++ assing1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "enter two numbers" << endl;
+    cin >> a >> b;
+    if (a > b)
+    {
+        cout << a << " is greater" << endl;
+    }
+    else if (a < b)
+    {
+        cout << b << " is greater" << endl;
+    }
+    else if (a == b)
+    {
+        cout << "both are equal" << endl;
+    }
+    else
+    {
+        cout << "error" << endl;
+    }
+}
